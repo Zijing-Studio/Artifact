43fdf0e547f2cc6d242c26dabf4d53f86b62260e
kxz-Manjaro <15068701650@163.com>
2019-11-08 20:56:48 +0800

    Add forbid adn select operation class

--- PlayerLegality/operation.py
+++ PlayerLegality/operation.py
@@ -1,3 +1,52 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+'''
+classes of operations
+'''
 
+class AbstractOperation:
+    '''
+    base class of all operations
+    '''
+    def __init__(self, _id):
+        self.player_id = _id
+
+    def check_legality(self):
+        '''
+        check legality of this operation
+        '''
+
+    def act(self):
+        '''
+        emit action event after legality check
+        '''
+
+class Forbid(AbstractOperation):
+    '''
+    operation of forbiding artifact and so on
+    '''
+    def __init__(self, _id, _params):
+        AbstractOperation.__init__(self, _id)
+        self.type = _params["type"]
+        self.target = _params["target"]
+
+    def check_legality(self):
+        return True
+
+    def act(self):
+        pass
+
+class Select(AbstractOperation):
+    '''
+    operation of selecting artifact and so on
+    '''
+    def __init__(self, _id, _params):
+        AbstractOperation.__init__(self, _id)
+        self.type = _params["type"]
+        self.target = _params["target"]
+
+    def check_legality(self):
+        return True
+
+    def act(self):
+        pass
--- PlayerLegality/player_legality.py
+++ PlayerLegality/player_legality.py
@@ -4,13 +4,17 @@
 '''
     player legality check api
 '''
+from operation import *
 
 def parse(operation):
     '''
         parse operation, check legality and emit responding event
     '''
     #create operation object
-    operation_object = toObject(operation)
+    operation_object = to_object(operation)
+    if operation_object == None:
+        #return error message
+        return ""
     if operation_object.check_legality:
         #emit responding event
         print("emit")
@@ -18,5 +22,25 @@ def parse(operation):
         #return error message
         print("error")
 
-def toObject(operation):
-    print("to")
+def to_object(operation_json):
+    '''
+    convert JSON to corresponding operation object
+    '''
+    try:
+        operation_type = operation_json["operation_type"].lower()
+        player_id = operation_json["player"]
+        params = operation_json["operation_parameters"]
+        if operation_type == "forbid":
+            operation_object = Forbid(player_id, params)
+        return operation_object
+    except KeyError as e:
+        print(e)
+
+if __name__ == "__main__":
+    example = {
+            "player": 1,
+            "operation_type": "Forbid",
+            "operation_parameters":{
+                }
+            }
+    parse(example)
