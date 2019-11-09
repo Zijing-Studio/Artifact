6f3911139a821de67c1ff97509400bd22b74c6cf
LastMC <last_mc@126.com>
2019-11-09 16:54:23 +0800

    Add sample archer class.

--- Unit.py
+++ Unit.py
@@ -3,7 +3,8 @@ Definition of unit classes
 '''
 
 class Unit:
-    def __init__(self,cost,atk,max_hp,atk_range,max_move,cool_down,pos,state_system):
+    def __init__(self,name,cost,atk,max_hp,atk_range,max_move,cool_down,pos,state_system):
+        self.name = name
         self.cost = cost
         self.atk = atk
         self.max_hp = max_hp
@@ -15,6 +16,26 @@ class Unit:
         self.state_system = state_system
         self.event_listener_list = []
 
+    def __str__(self):
+        return '''{}
+    Cost: {}
+    Atk: {}
+    HP: {}/{}
+    Atk Range: {}
+    Max Move: {}
+    Cool Down:{}
+    Pos: {}'''.format(
+                self.name,
+                self.cost,
+                self.atk,
+                self.hp,
+                self.max_hp,
+                self.atk_range,
+                self.max_move,
+                self.cool_down,
+                self.pos
+            )
+
     def add_event_listener(self,listener):
         self.event_listener_list.append(listener)
 
@@ -24,3 +45,29 @@ class Unit:
 
     def emit(self,event):
         self.state_system.emit(event)
+
+class Archer(Unit):
+    def __init__(self,level,pos,state_system):
+        name = "Archer"
+        cost = [2,4,6]
+        atk = [1,2,3]
+        hp = [1,3,5]
+        atk_range = (2,4)
+        max_move = 3
+        cool_down = 4
+        Unit.__init__(
+            self,
+            name + " (Level " + str(level) + ")",
+            cost[level-1],
+            atk[level-1],
+            hp[level-1],
+            atk_range,
+            max_move,
+            cool_down,
+            pos,
+            state_system
+        )
+
+if __name__=="__main__":
+    archer = Archer(2,(0,0,0,0),None)
+    print(archer)
