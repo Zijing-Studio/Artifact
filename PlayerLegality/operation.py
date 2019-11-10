d62db41090c74d9bd8cd1953bb6bd9dd7ec3b261
kxz-Manjaro <15068701650@163.com>
2019-11-10 17:14:45 +0800

    Initialize

--- PlayerLegality/operation.py
+++ PlayerLegality/operation.py
@@ -50,3 +50,65 @@ class Select(AbstractOperation):
 
     def act(self):
         pass
+
+class Summon(AbstractOperation):
+    '''
+    summon creature
+    '''
+    def __init__(self, _id, _params):
+        AbstractOperation.__init__(self, _id)
+        self.type = _params["type"]
+        self.star = _params["star"]
+        self.position = _params["position"]
+
+    def check_legality(self):
+        return True
+
+    def act(self):
+        pass
+
+class Move(AbstractOperation):
+    '''
+    move creature
+    '''
+    def __init__(self, _id, _params):
+        AbstractOperation.__init__(self, _id)
+        self.mover = _params["mover"]
+        self.position = _params["position"]
+
+    def check_legality(self):
+        return True
+
+    def act(self):
+        pass
+
+class Attack(AbstractOperation):
+    '''
+    attack operation
+    '''
+    def __init__(self, _id, _params):
+        AbstractOperation.__init__(self, _id)
+        self.attacker = _params["attacker"]
+        self.target = _params["target"]
+
+    def check_legality(self):
+        return True
+    
+    def act(self):
+        pass
+
+class Use(AbstractOperation):
+    '''
+    use artifact or trap card
+    '''
+    def __init__(self, _id, _params):
+        AbstractOperation.__init__(self, _id)
+        self.type = _params["type"]
+        self.card = _params["card"]
+        self.target = _params["target"]
+
+    def check_legality(self):
+        return True
+
+    def act(self):
+        pass
