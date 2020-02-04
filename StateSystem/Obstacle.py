ddbee3a6c520b15b35d86dfa32a0b6b27abfd85a
LastMC <last_mc@126.com>
2020-02-04 15:58:43 +0800

    Add obstacle type difference

--- StateSystem/Obstacle.py
+++ StateSystem/Obstacle.py
@@ -3,12 +3,14 @@ class Obstacle:
         self.type = name
         self.pos = pos
         self.allow_flying = name == "Abyss"
+        self.allow_ground = False
 
     def parse(self):
         return {
             "type": self.type,
             "pos": self.pos,
-            "allow_flying": self.allow_flying
+            "allow_flying": self.allow_flying,
+            "allow_ground": self.allow_ground
         }
 
 ABYSS_INIT_LIST = [
