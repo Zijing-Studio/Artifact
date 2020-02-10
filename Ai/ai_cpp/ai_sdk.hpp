a1eb81d7e26d5bb50476c73437fdfb3c82d8bce5
hsr <hsr17@mails.tsinghua.edu.cn>
2020-02-10 12:04:04 +0800

    make some move

--- Ai/ai_cpp/ai_sdk.hpp
+++ Ai/ai_cpp/ai_sdk.hpp
@@ -4,12 +4,14 @@
 #include <vector>
 #include <iostream>
 
+#include "calculator.h"
+#include "gameunit.hpp"
 #include "json.hpp"
 using json = nlohmann::json;
 
 namespace ai_sdk
 {
-
+//通信部分
 void sendLen(std::string s)
 {
     int len = s.length();
@@ -116,6 +118,129 @@ void endRound(int round)
 {
     json operation_parameters;
     sendMsg(round, "endround", operation_parameters);
+}
+
+//查询部分
+
+// 己方单位从位置pos_a到位置pos_b的地面距离(考虑被敌方地面生物阻挡但不考虑被敌方生物拦截)
+int getDistanceOnGround(gameunit::Map map, gameunit::Pos pos_a, gameunit::Pos pos_b, int my_camp)
+{
+    //地图边界
+    std::vector<gameunit::Pos> obstacles_pos = calculator::MAPBORDER();
+    //地面障碍
+    for (auto p = map.ground_obstacles.begin(); p != map.ground_obstacles.end(); p++)
+    {
+        obstacles_pos.push_back(p->pos);
+    }
+    //敌方地面生物
+    for (auto p = map.units.begin(); p != map.units.end(); p++)
+    {
+        if (p->camp != my_camp && (!p->flying))
+        {
+            obstacles_pos.push_back(p->pos);
+        }
+    }
+    return calculator::search_path(pos_a, pos_b, obstacles_pos, {}).size();
+}
+
+// 己方单位从位置pos_a到位置pos_b的飞行距离(考虑被敌方飞行生物阻挡但不考虑被敌方生物拦截)
+int getDistanceInSky(gameunit::Map map, gameunit::Pos pos_a, gameunit::Pos pos_b, int my_camp)
+{
+    //地图边界
+    std::vector<gameunit::Pos> obstacles_pos = calculator::MAPBORDER();
+    //飞行障碍
+    for (auto p = map.flying_obstacles.begin(); p != map.flying_obstacles.end(); p++)
+    {
+        obstacles_pos.push_back(p->pos);
+    }
+    //敌方飞行生物
+    for (auto p = map.units.begin(); p != map.units.end(); p++)
+    {
+        if (p->camp != my_camp && p->flying)
+        {
+            obstacles_pos.push_back(p->pos);
+        }
+    }
+    return calculator::search_path(pos_a, pos_b, obstacles_pos, {}).size();
+}
+
+// 对于指定位置pos,获取其上所有生物
+std::vector<gameunit::Unit> getUnits(gameunit::Map map, gameunit::Pos pos)
+{
+    std::vector<gameunit::Unit> units_on_pos;
+    for (int i = 0; i < map.units.size(); ++i)
+    {
+        if (map.units[i].pos == pos)
+            units_on_pos.push_back(map.units[i]);
+    }
+    return units_on_pos;
+}
+
+// 对于指定位置pos,判断其驻扎情况
+// 不是驻扎点返回-2,中立返回-1,否则返回占领该驻扎点的阵营(0/1)
+int checkBarrack(gameunit::Map map, gameunit::Pos pos)
+{
+    for (auto barrack = map.barracks.begin(); barrack != map.barracks.end(); barrack++)
+    {
+        if (barrack->pos == pos)
+            return barrack->camp;
+    }
+    return -2;
+}
+
+// 判断生物attacker能否攻击到生物target(只考虑攻击力、攻击范围)
+bool canAttack(gameunit::Unit attacker, gameunit::Unit target)
+{
+    //攻击力小于等于0的单位无法攻击
+    if (attacker.atk <= 0)
+        return false;
+    //攻击范围
+    int dist = calculator::cube_distance(attacker.pos, target.pos);
+    if (dist < attacker.atk_range.first || dist > attacker.atk_range.second)
+        return false;
+    //对空攻击
+    if (target.flying && (!attacker.atk_flying))
+        return false;
+    return true;
+}
+
+// 判断能否对位置pos使用神器artifact(不考虑消耗、冷却)
+bool canUseArtifact(gameunit::Map map, gameunit::Artifact artifact, gameunit::Pos pos, int my_camp)
+{
+    if (artifact.name == "HolyLight")
+    {
+        return calculator::in_map(pos);
+    }
+    else if (artifact.name == "InfernoFlame")
+    {
+        // 无地面生物
+        for (auto unit = map.units.begin(); unit != map.units.end(); unit++)
+        {
+            if ((unit->pos == pos) && (not unit->flying))
+                return false;
+        }
+        // 距己方神迹范围<=5
+        if (calculator::cube_distance(pos, map.relics[my_camp].pos) <= 5)
+            return true;
+        // 距己方占领驻扎点范围<=3
+        for (auto barrack = map.barracks.begin(); barrack != map.barracks.end(); barrack++)
+        {
+            if ((barrack->camp == my_camp) && (calculator::cube_distance(pos, barrack->pos) <= 3))
+                return true;
+        }
+    }
+    return false;
+}
+
+// 判断能否对生物pos使用神器artifact(不考虑消耗、冷却)
+bool canUseArtifact(gameunit::Artifact artifact, gameunit::Unit unit)
+{
+    if (artifact.name == "SalamanderShield")
+    {
+        // 己方生物
+        return artifact.camp == unit.camp;
+    }
+    return false;
 }
 }; // namespace ai_sdk
 
--- Ai/ai_py/ai_sdk.py
+++ Ai/ai_py/ai_sdk.py
@@ -3,6 +3,7 @@
 
 import sys
 import json
+import calculator
 
 
 def convert_byte(data_str):
@@ -17,6 +18,7 @@ def convert_byte(data_str):
     message += bytes(data_str, encoding="utf8")
     return message
 
+
 def send_opt(data_str):
     '''发送操作
 
@@ -28,6 +30,7 @@ def send_opt(data_str):
     sys.stdout.buffer.write(convert_byte(data_str))
     sys.stdout.flush()
 
+
 def read_opt():
     '''读取发过来的操作
 
@@ -37,6 +40,7 @@ def read_opt():
     data_length = read_buffer.read(4)
     data = read_buffer.read(int(data_length.decode()))
     return json.loads(data)
+
 
 def init(artifacts, creatures):
     '''选择初始神器和生物
@@ -57,6 +61,7 @@ def init(artifacts, creatures):
         }
     }
     send_opt(json.dumps(message))
+
 
 def summon(_round, _type, star, position):
     '''在地图position处召唤一个本方类型为_type，星级为star的单位
@@ -84,6 +89,7 @@ def summon(_round, _type, star, position):
     }
     send_opt(json.dumps(message))
 
+
 def move(_round, mover, position):
     '''将地图上id为mover的单位移动到地图position处
 
@@ -105,6 +111,7 @@ def move(_round, mover, position):
         }
     }
     send_opt(json.dumps(message))
+
 
 def attack(_round, attacker, target):
     '''令地图上id为attacker的单位攻击地图上id为target的单位
@@ -128,6 +135,7 @@ def attack(_round, attacker, target):
     }
     send_opt(json.dumps(message))
 
+
 def use(_round, artifact, target):
     '''对target目标使用artifact神器
 
@@ -150,6 +158,7 @@ def use(_round, artifact, target):
     }
     send_opt(json.dumps(message))
 
+
 def end_round(_round):
     '''结束当前回合
 
@@ -162,3 +171,87 @@ def end_round(_round):
         "operation_type": "endround"
     }
     send_opt(json.dumps(message))
+
+
+def get_distance_on_ground(_map, pos_a, pos_b, my_camp):
+    '''获取己方单位从位置pos_a到位置pos_b的地面距离
+    '''
+    # 地图边界
+    obstacles_pos = calculator.MAPBORDER[:]
+    # 地面障碍
+    obstacles_pos += _map["ground_obstacles"][:]
+    # 敌方地面生物
+    for unit in _map["units"]:
+        if unit["camp"] != my_camp and (not unit["flying"]):
+            obstacles_pos.append(unit["pos"])
+    return len(calculator.search_path(pos_a, pos_b, obstacles_pos, []))
+
+
+def get_distance_in_sky(_map, pos_a, pos_b, my_camp):
+    '''获取己方单位从位置pos_a到位置pos_b的飞行距离
+    '''
+    # 地图边界
+    obstacles_pos = calculator.MAPBORDER[:]
+    # 地面障碍
+    obstacles_pos += _map["flying_obstacles"][:]
+    # 敌方地面生物
+    for unit in _map["units"]:
+        if unit["camp"] != my_camp and unit["flying"]:
+            obstacles_pos.append(unit["pos"])
+    return len(calculator.search_path(pos_a, pos_b, obstacles_pos, []))
+
+
+def get_units(_map, pos):
+    '''对于指定位置pos,获取其上所有生物
+    '''
+    return [unit for unit in _map["units"] if unit["pos"] == pos]
+
+
+def check_barrack(_map, pos):
+    '''对于指定位置pos,判断其驻扎情况
+
+    不是驻扎点返回-2,中立返回-1,否则返回占领该驻扎点的阵营(0/1)
+    '''
+    for barrack in _map["barracks"]:
+        if barrack["pos"] == pos:
+            return barrack["camp"]
+    return -2
+
+
+def can_attack(attacker, target):
+    '''判断生物attacker能否攻击到生物target(只考虑攻击力、攻击范围)
+    '''
+    # 攻击力小于等于0的单位无法攻击
+    if attacker["atk"] <= 0:
+        return False
+    # 攻击范围
+    dist = calculator.cube_distance(attacker["pos"], target["pos"])
+    if dist < attacker["atk_range"][0] or dist > attacker["atk_range"][1]:
+        return False
+    # 对空攻击
+    if target["flying"] and (not attacker["atk_flying"]):
+        return False
+    return True
+
+
+def can_use_artifact(_map, artifact, target, my_camp):
+    '''判断能否对目标target使用神器artifact(不考虑消耗、冷却)
+    '''
+    if artifact["name"] == "HolyLight":
+        return calculator.in_map(target)
+    if artifact["name"] == "InfernoFlame":
+        # 无地面生物
+        for unit in _map["units"]:
+            if (unit["pos"] == target) and (not unit["flying"]):
+                return False
+        # 神迹范围<=5
+        if calculator.cube_distance(target, _map["relics"][my_camp]["pos"]) <= 5:
+            return True
+        # 占领驻扎点范围<=3
+        for barrack in _map["barracks"]:
+            if (barrack["camp"] == my_camp and
+                    calculator.cube_distance(target, barrack["pos"]) <= 3):
+                return True
+    elif artifact["name"] == "SalamanderShield":
+        return artifact["camp"] == target["camp"]
+    return False
